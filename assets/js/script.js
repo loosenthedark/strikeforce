96dfee4c96212da02321e32dee66c9f4af927db0
paul Harrington <paulharrington@pauls-MacBook-Pro-4.local>
2021-02-12 00:53:59 +0000

    Style hover states of club button elemets

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,7 +104,7 @@ const showClubs = matches => {
           <span class="statcard-desc pt-1 statcard-desc-${match.clubname.replace(/\s/g, '').replace('&', 'and').toLowerCase()}">#${match.acronym}</span></div>
           <div class="statcard col-2 d-flex statcard-crest statcard-crest-${match.clubname.replace(/\s/g, '').replace('&', 'and').toLowerCase()} pl-1 pr-2 py-2 mb-1">
           <img class="club-crest" src="${match.crestUrl}" height="48px" width="100%">
-        </div><div class="statcard col-2 d-flex px-2 mb-1 statcard-line-chart statcard-line-chart-${match.clubname.replace(/\s/g, '').replace('&', 'and').toLowerCase()}">
+        </div><div class="statcard col-2 d-flex px-0 mb-1 statcard-line-chart statcard-line-chart-${match.clubname.replace(/\s/g, '').replace('&', 'and').toLowerCase()}">
         <a href="#" class="d-flex btn-strike-rate-club btn-strike-rate-club-${match.clubname.replace(/\s/g, '').replace('&', 'and').toLowerCase()}" title="Load club dashboards"><i class="d-flex fas fa-chart-line"></i></a>
       </div></div>
       </div>`).join('');
