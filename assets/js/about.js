df8b43d62a8297634f49a7b1017583295fa7acff
paul Harrington <paulharrington@pauls-MacBook-Pro-4.local>
2021-03-09 12:13:09 +0000

    Apply styles for all pages on tablet and desktop and refactor main nav bar for About and Contact pages from tablet up

--- assets/js/about.js
+++ assets/js/about.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
         $('.nav-toggler-icon').toggleClass('active');
         $('#nav-toggler').toggleClass('no-border');
     });
+    $('.dropdown-toggle').click(function() {
+        if (this.classList.contains('clicked')) {
+            $(this).removeClass('clicked');
+        } else {
+            $('.clicked').toggleClass('clicked');
+            $(this).addClass('clicked');
+        }
+    });
+    $("#content-main").click(function() {
+        $('.dropdown-toggle').removeClass('clicked');
+    });
 });
